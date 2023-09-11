2e79279bd0608e92638d2085defa76aa8f360353
Manjinder <smanjinder96.ms@gmail.com>
2023-09-11 13:53:46 -0700

    db changes

--- backend/Database/sequalizeConnection.js
+++ backend/Database/sequalizeConnection.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_NAME_DEV, process.env.DATABASE_USER_DEV, process.env.DATABASE_PWD_DEV, {
     host: process.env.DATABASE_HOST_DEV,
     dialect: 'mysql',
+    dialectModule: require('mysql2'),
     dialectOptions: {
         ssl: {
             rejectUnauthorized: true,
