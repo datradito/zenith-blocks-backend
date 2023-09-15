8d368fa5371d617f6e7a5468ae3d27af6dcb1854
Manjinder <smanjinder96.ms@gmail.com>
2023-09-14 22:24:55 -0700

    testsssssUpdate index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: 'https://zenithblocksfrontend.vercel.app/' }));
+app.use(cors({ credentials: true, origin: 'https://zenithblocksfrontend.vercel.app' }));
 
 const hour = 3600000;
 
