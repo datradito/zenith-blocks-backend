8186fdba026145372448397c00ff5a3b48f82802
Manjinder <smanjinder96.ms@gmail.com>
2023-10-17 21:00:05 -0700

    merge into main

--- frontend/src/Components/DisplayElements/Header/WalletConnect.jsx
+++ frontend/src/Components/DisplayElements/Header/WalletConnect.jsx
@@ -11,12 +11,6 @@ import { useDispatch } from "react-redux";
 import { setIsLoggedIn } from "../../../actions/createAuthAction";
 import { toast } from "react-hot-toast";
 
-const BASE_URL = process.env.REACT_APP_API_URL;
-// const BASE_URL = "http://localhost:8000";
-
-const BASE_URL = process.env.REACT_APP_API_URL;
-// const BASE_URL = "http://localhost:8000";
-
 const clearAuthData = () => {
   sessionStorage.removeItem("authToken");
   sessionStorage.removeItem("address");
