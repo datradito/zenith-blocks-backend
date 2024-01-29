8f2bc85c968501673c0963015ed14003bcb634d7
Manjinder <smanjinder96.ms@gmail.com>
2024-01-28 17:32:38 -0800

    ZB-16: hard coded gas limit for now

--- frontend/src/Components/hooks/Payments/useHandleTransfer.jsx
+++ frontend/src/Components/hooks/Payments/useHandleTransfer.jsx
@@ -1,31 +1,62 @@
 import { useCallback } from "react";
 import { useWalletClient } from "wagmi";
 
-export default function useHandleTransfer() {
-    const { data: walletClient, isError, isLoading } = useWalletClient();
-    
-    return useCallback((transactionConfig) => {
-      if (isError) {
-        return {
-          error: {
-            shortMessage: "Error: Wallet client not found",
-          },
-        };
-      }
-        if (isLoading) {
-            return {
-            error: {
-                shortMessage: "Error: Wallet client is loading",
-            },
-            };
-        }
+/**
+ * Initiates a token transfer transaction using the connected wallet client.
+ * 
+ * @param {Object} transferConfig - The transfer config object
+ * @param {string} transferConfig.contractAddress - The contract address 
+ * @param {Object} transferConfig.contractABI - The contract ABI
+ * @param {string} transferConfig.recipient - The recipient address
+ * @param {string} transferConfig.amount - The transfer amount 
+ * @returns {Promise} - Promise that resolves to the transaction result or rejects with an error
+ */
+export default function useHandleTransfer(transferConfig) {
 
-      
-      return walletClient.writeContract({
-      address: transactionConfig.contractAddress,
-      abi: transactionConfig.contractABI,
-      functionName: "transfer",
-      args: [transactionConfig.address, transactionConfig.amount],
-    });
-  }, [walletClient]);
+  const { data: walletClient, isError, isLoading } = useWalletClient();
+
+  return useCallback(async (transferConfig) => {
+
+    if (isError) {
+      return {
+        error: {
+          shortMessage: "Error: Wallet client not found"
+        }
+      };
+    }
+
+    if (isLoading) {
+      return {
+        error: {
+          shortMessage: "Error: Wallet client is loading"
+        }
+      };
+    }
+
+    if (!transferConfig.contractAddress || !transferConfig.contractABI) {
+      return {
+        error: {
+          shortMessage: "Required transfer config fields missing"
+        }
+      }
+    }
+
+    try {
+      return await walletClient.writeContract({
+        address: transferConfig.contractAddress,
+        abi: transferConfig.contractABI,
+        functionName: "transfer",
+        args: [transferConfig.address, transferConfig.amount * 10 ** 6],
+        gas: process.env.REACT_APP_GAS_LIMIT,
+      });
+    } catch (error) {
+      return {
+        error: {
+          shortMessage: error.message
+        }
+      };
+    }
+
+  }, [isError, isLoading, walletClient]);
+
 }
