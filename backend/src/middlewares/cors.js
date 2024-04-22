f2cf526d486ab4ae24d46efd52f367e87460762d
Daniel Agustin Tradito <datradito@gmail.com>
2024-04-22 10:48:12 -0300

    ZB-42: Refactor corsOptions in cors.js middleware to allow multiple origins

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,10 +1,16 @@
-
 export const corsOptions = {
   credentials: true,
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      process.env.FRONTEND_URL,
+      process.env.DEV_CORS_ALLOWED_URL,
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   optionsSuccessStatus: 200,
 };
-
-
-
